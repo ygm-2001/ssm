e4377f6027e1e77e5d7449254c9d68ddf45ecaef
ygm-2001 <484492319@qq.com>
2020-12-19 10:58:56 +0800

    内容：对'User'的修改； 功能：API(增加)； 负责人：ygm;

--- src/main/java/com/zking/ssm/controller/UserController.java
+++ src/main/java/com/zking/ssm/controller/UserController.java
@@ -4,6 +4,7 @@ import com.zking.ssm.model.User;
 import com.zking.ssm.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -14,15 +15,29 @@ public class UserController {
     private IUserService userService;
 
     @RequestMapping("/login")
-    public void Login(){
+    public String Login(Model model){
         System.out.println("登录---login");
         User user = userService.selectByPrimaryKey(1L);
-        System.out.println(user);
-    }
-
-    @RequestMapping("/login2")
-    public String Login2(){
-        System.out.println("登录...login2");
+        model.addAttribute("user",user);
         return "index";
     }
+
+    @RequestMapping("/add")
+    public String Add(Model model,User user){
+        String mgs = "未";
+        if(null!=user.getName() && !"".equals(user.getName())){
+            int i = userService.insertSelective(user);
+            if(1==i){
+                mgs = "注册成功";
+                model.addAttribute("addUser",user);
+            }else{
+                mgs="注册失败";
+            }
+        }else{
+            mgs = "用户名不能为空";
+        }
+        model.addAttribute("mgs",mgs);
+        return "index";
+    }
+
 }
